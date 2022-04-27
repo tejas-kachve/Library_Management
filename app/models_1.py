bdcfdd5f2faa4e2320d0c3772388833533c6631e
Tejas.Kachve <tejas-kachve>
2022-04-27 17:36:44 +0530

    Message:First Commit

--- app/models_1.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    book_name = models.CharField(max_length = 1000 , null = True , blank = True)
-    price     = models.FloatField(null = True , blank = True)
-    author    = models.CharField(max_length= 1000 , null = True , blank=True)
-    description  = models.TextField(null = True , blank = True)
--- app/urls_1.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns = [
-    path("",views.home , name = "home"),
-    path("create_book",views.create_book , name = "create_book"),
-    path("delete_book/<int:id>",views.delete_book , name = "delete_book"),
-    path("update_book/<int:id>",views.update_book , name = "update_book")
-]
