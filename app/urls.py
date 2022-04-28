567a9dc1dffdd1199808efda550e98aa5cfdd429
tejas-kachve <101544287+tejas-kachve@users.noreply.github.com>
2022-04-28 12:21:36 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from.import views
 
 urlpatterns = [
     path("",views.home , name = "home"),
-    path("create_book",views.create_book , name = "create_book")
+    path("create_book",views.create_book , name = "create_book"),
+    path("delete_book/<int:id>",views.delete_book , name = "delete_book"),
+    path("update_book/<int:id>",views.update_book , name = "update_book")
 ]
